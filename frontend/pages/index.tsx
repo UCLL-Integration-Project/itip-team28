189c9809a413b3acda4d94add5e02842dfda4a22
Nina de Weerd <nina.deweerd@student.ucll.be>
2025-05-13 16:27:27 +0200

    frontend setup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="BowlBuddies Pokebowl app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="assets/logo.png" />
+            </Head>
+
+            <main className='homepage-main'>
+                <section className='homepage-section'>
+                    <h1>Hello world</h1>
+                </section>
+            </main>
+        </>
+    );
+};
+
+
+export default Home;
