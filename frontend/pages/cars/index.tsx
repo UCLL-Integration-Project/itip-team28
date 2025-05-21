5f88fea65f314a25f51b45ff8dca0a1feee20435
Nina de Weerd <nina.deweerd@student.ucll.be>
2025-05-21 16:38:33 +0200

    merged code

--- frontend/pages/cars/index.tsx
+++ frontend/pages/cars/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import CarOverview from '@/components/CarOverview';
+import CarOverview from '@/components/cars/CarOverview';
 import { Car } from '@/types';
 import CarService from '@/services/CarService';
 import Header from '@/components/header';
+import Head from 'next/head';
 
 const CarNavigationPage: React.FC = () => {
     const [cars, setCars] = useState<Car[]>([]);
@@ -36,12 +37,20 @@ const CarNavigationPage: React.FC = () => {
 
     return (
         <>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="Car app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="assets/logo.png" />
+            </Head>
             <Header />
-            <div>
-                <h1>Navigation</h1>
-                {error && <div style={{ color: 'red' }}>{error}</div>}
-                <CarOverview cars={cars} />
-            </div>
+
+            <main className="flex flex-col items-center justify-center min-h-screen">
+                <section>
+                    {error && <div className="text-red-500">{error}</div>}
+                    <CarOverview cars={cars} />
+                </section>
+            </main>
         </>
     );
 };
